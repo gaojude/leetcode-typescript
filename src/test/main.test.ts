b9992683a0be15298e63e5768aeabe77b154583d
gaojude <gaojude@outlook.com>
2020-10-15 20:10:06 -0400

    daily challenge added

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -1,4 +1,4 @@
-import {search, findMinArrowShots, removeDuplicateLetters, buddyStrings} from '../main'
+import { search, findMinArrowShots, removeDuplicateLetters, buddyStrings, ListNode, sortList, rob, rotate } from '../main'
 import assert from 'assert'
 
 describe('Binary search simple test', () => {
@@ -12,19 +12,19 @@ describe('Binary search simple test', () => {
 
 describe('findMinArrowShots sample tests', () => {
     it('sample test 1', () => {
-        assert(findMinArrowShots([[10,16],[2,8],[1,6],[7,12]]) === 2)
+        assert(findMinArrowShots([[10, 16], [2, 8], [1, 6], [7, 12]]) === 2)
     })
     it('sample test 2', () => {
-        assert(findMinArrowShots([[1,2],[3,4],[5,6],[7,8]]) === 4)
+        assert(findMinArrowShots([[1, 2], [3, 4], [5, 6], [7, 8]]) === 4)
     })
     it('sample test 3', () => {
-        assert(findMinArrowShots([[1,2],[2,3],[3,4],[4,5]]) === 2)
+        assert(findMinArrowShots([[1, 2], [2, 3], [3, 4], [4, 5]]) === 2)
     })
     it('sample test 4', () => {
-        assert(findMinArrowShots([[1,2]]) === 1)
+        assert(findMinArrowShots([[1, 2]]) === 1)
     })
     it('sample test 5', () => {
-        assert(findMinArrowShots([[2,3],[2,3]]) === 1)
+        assert(findMinArrowShots([[2, 3], [2, 3]]) === 1)
     })
 })
 
@@ -52,5 +52,63 @@ describe('buddyStrings tests', () => {
     })
     it('test 5', () => {
         assert(!buddyStrings('', 'aa'));
+    })
+})
+
+function arrayToListNode(array: number[]): ListNode | null {
+    if (array.length === 0) {
+        return null;
+    }
+    const [head, ...rest] = array;
+    return new ListNode(head, arrayToListNode(rest));
+}
+
+function listNodeToArray(node: ListNode | null): number[] {
+    if (node === null) {
+        return [];
+    }
+    return [node.val].concat(listNodeToArray(node.next));
+}
+
+function arrayEqual(array1: number[], array2: number[]): boolean {
+    return array1.toString() === array2.toString();
+}
+
+describe('sortList tests', () => {
+    it('test 1', () => {
+        assert(arrayEqual(listNodeToArray(sortList(arrayToListNode([-1, 5, 3, 4, 0]))), [-1, 0, 3, 4, 5]))
+    })
+    it('test 2', () => {
+        assert(arrayEqual(listNodeToArray(sortList(arrayToListNode([]))), []))
+    })
+    it('test 3', () => {
+        assert(arrayEqual(listNodeToArray(sortList(arrayToListNode([4, 2, 1, 3]))), [1, 2, 3, 4]))
+    })
+})
+
+describe('rob tests', () => {
+    it('test 1', () => {
+        assert(rob([2, 3, 2]) === 3)
+    })
+    it('test 2', () => {
+        assert(rob([1, 2, 3, 1]) === 4)
+    })
+    it('test 3', () => {
+        assert(rob([0]) === 0)
+    })
+})
+
+describe('rotate tests', () => {
+    it('test 1', () => {
+        const arr = [1, 2, 3, 4, 5, 6, 7]
+        const k = 3
+        rotate(arr, k);
+        assert(arrayEqual(arr, [5, 6, 7, 1, 2, 3, 4]))
+    })
+    it('test 2', () => {
+        const arr = [-1, -100, 3, 99]
+        const k = 2
+        rotate(arr, k);
+        assert(arrayEqual(arr, [3, 99, -1, -100]))
     })
 })
